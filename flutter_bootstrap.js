c14b573d1d9349ca62f2ce4b84f7ec5588dc6f9b
Sanoop Das <lonewolf.sddb@gmail.com>
2024-11-19 18:19:08 +0530

    update 18 files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2174271644"
+    serviceWorkerVersion: "2046526711"
   }
 });
